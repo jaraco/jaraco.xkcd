30d3669a096143031b374acf4eb9a2568422ffea
Jason R. Coombs <jaraco@jaraco.com>
2018-04-30 14:37:05 -0400

    Python 3 only

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ params = dict(
 		name.split('.')[:-1] if nspkg_technique == 'managed'
 		else []
 	),
-	python_requires='>=2.7',
+	python_requires='>=3.5',
 	install_requires=[
 		'requests',
 	],
